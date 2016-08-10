a26dbfa0c99d3c609a496c75edd424c5e7d490cb
sunskyhsh <sunskyhsh@hotmail.com>
2016-08-10 21:39:31 +0800

    full project implement

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import sqlite3
-from flask import Flask, render_template, redirect, request, session, url_for, abort, flash
+from flask import g, Flask, render_template, redirect, request, session, url_for, abort, flash
 from contextlib import closing
 
 # 配置文件
@@ -32,7 +32,7 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-	cur = g.db.execute('select title, text from entries or der by id desc')
+	cur = g.db.execute('select title, text from entries order by id desc')
 	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
 	return render_template('show_entries.html', entries=entries)
 
@@ -45,11 +45,25 @@ def add_entry():
 	flash('Newe entry was successfully posted')
 	return redirect(url_for('show_entries'))
 
-@app.route('login', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
 	error = None
 	if request.method == 'POST':
-		if 
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
 	app.run()
